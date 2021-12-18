9c9bc2a1abb6419ace875c9cc47acd1cc1c0fd8c
Yohannes Wondimu <yohannes.wondimu@gmail.com>
2021-12-18 10:47:19 -0400

    Character - added block methods, other small revs

--- Character.java
+++ Character.java
@@ -2,11 +2,14 @@ public abstract class Character {
 	private int health;
 	private int mana;
 	private	final String type;
+	private boolean isBlocking;
 
 	public Character (int health, int mana, String type) {
 		this.health = health;
 		this.mana = mana;
 		this.type = type;
+
+		this.isBlocking = false;
 	}
 
 
@@ -20,20 +23,34 @@ public abstract class Character {
 	public String getType() {
 		return type;
 	}
+	public boolean isBlocking() {
+		return isBlocking;
+	}
 
 	//setters
-	public void setHealth(int amount) {
+	public void setHealthTo(int amount) {
 		health = amount;
 	}
-	public void setMana(int amount) {
+	public void setManaTo(int amount) {
 		mana = amount;
 	}
+	public void setBlockingTo(boolean blockStatus) {
+		isBlocking = blockStatus;
+	}
+	public void block() {
+		isBlocking = true;
+	}
+	public void unBlock() {
+		isBlocking = false;
+	}
+
 
 	//other methods
-	public void attack(Character target, int amount) {
+	public void attack(Character target, int amount, String attackType) {
 		int health = target.getHealth();
 		target.setHealth(health-amount);
 	}
+
 	public void healSelfBy(int amount) {
 		health += amount;
 	}
