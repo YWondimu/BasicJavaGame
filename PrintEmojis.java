b69e799a1d714d1de313360d16084188559b0997
Yohannes Wondimu <yohannes.wondimu@gmail.com>
2021-12-18 19:38:56 -0400

    Make PrintEmojis file
    --WHAT--
    Make file that has print statements of emojis.
    These statements used to be commented out statements in another file.
    
    --WHY--
    So that I know what the emojis look like.

---
+++ PrintEmojis.java
@@ -0,0 +1,18 @@
+public class PrintEmojis {
+	public static void main (String[] args) {
+		System.out.println("Fairy: \uD83E\uDDDA\u200D\u2642\u2B05\uFE0F");
+		System.out.println("Fairy: \uD83E\uDDDA\u200D\u2642\u27A1\uFE0F");
+		System.out.println("Genie: \uD83E\uDDDE\u200D\u2642");
+		System.out.println("Zombie: \uD83E\uDDDA\u200D\u2642");
+		System.out.println("Wizard: \uD83E\uDDD9\u200D\u2642");
+		System.out.println("Skeleton: \uD83E\uDDDA\u200D\u2642");
+		System.out.println("Face Left: \u1F3F3\u200D\u2B05\uFE0F");
+		System.out.println("Face Right: \u1F3F3\u200D\u27A1\uFE0F");
+		System.out.println("Fairy: \uD83E\uDDDA\u200D\u2642");
+		System.out.println("Fairy: \uD83E\uDDDA\u200D\u2642");
+		System.out.println("Fencing Left: \uD83E\uDD3A\u2B05\uFE0F");
+		System.out.println("Fencing Right: \uD83E\uDD3A\u27A1\uFE0F");
+		System.out.println("Ninja: \uD83E\uDD77\uD83C\uDFFC");
+		System.out.println("Vampire: \uD83E\uDDDB\u200D\u2640");
+	}
+}
